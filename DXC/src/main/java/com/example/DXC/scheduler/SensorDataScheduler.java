3e21284b1e6f4586d13277f7154fd17046b9357b
IbrahimM0stafa <ibrahimmostafa768@gmail.com>
2025-05-25 02:50:14 +0300

    Update: [Ports Adjustments]

--- DXC/src/main/java/com/example/DXC/scheduler/SensorDataScheduler.java
+++ DXC/src/main/java/com/example/DXC/scheduler/SensorDataScheduler.java
@@ -11,7 +11,7 @@ public class SensorDataScheduler {
 
     private final SensorDataService sensorDataService;
 
-    @Scheduled(fixedRate = 600000)
+    @Scheduled(fixedRate = 60000)
     public void generateAllSensorData() {
         sensorDataService.generateTrafficData();
         sensorDataService.generateAirPollutionData();
