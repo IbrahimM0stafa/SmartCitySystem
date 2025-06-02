c8addbda3ca2a2f34c1d6c1769760695ce397f3e
Youssef Ashraf <114014804+Youssef-Ashraf02@users.noreply.github.com>
2025-06-02 22:11:03 +0300

    features implementation + design patterns

--- DXC/src/main/java/com/example/DXC/service/UserService.java
+++ DXC/src/main/java/com/example/DXC/service/UserService.java
@@ -2,6 +2,7 @@ package com.example.DXC.service;
 
 import com.example.DXC.dto.SignupRequest;
 import com.example.DXC.dto.SignInRequest;
+import com.example.DXC.dto.UpdateProfileRequest;
 import com.example.DXC.dto.UserProfileResponse;
 import com.example.DXC.model.User;
 
@@ -9,4 +10,5 @@ public interface UserService {
     User registerUser(SignupRequest request);
 //    User loginUser(SignInRequest request);
     UserProfileResponse getProfile(Long userId);
+    UserProfileResponse updateProfile(Long userId, UpdateProfileRequest updateRequest);
 }
